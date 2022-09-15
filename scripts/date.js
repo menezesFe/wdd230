4033f4a71fe260c2b50562f1ca4078379394382d
menezesFe <felipe.menezes6@outlook.com>
2022-09-15 14:16:28 -0400

    Create list with empty links

---
+++ scripts/date.js
@@ -0,0 +1,30 @@
+let dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+let d = new Date();
+let dayName = dayNames[d.getDay()];
+let monthName = months[d.getMonth()];
+let fullDate = dayName + ", " + monthName + " " + d.getDate() + ", " + d.getFullYear();
+
+document.getElementById("currentDate").textContent = fullDate;
