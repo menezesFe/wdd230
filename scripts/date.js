44ea3aa60030e3ccefce4e4472f850d6ac8240fc
menezesFe <felipe.menezes6@outlook.com>
2022-09-16 20:35:51 -0400

    Modify selector for currentYear

--- scripts/date.js
+++ scripts/date.js
@@ -23,9 +23,11 @@ let months = [
 ];
 
 let d = new Date(document.lastModified);
+let year = d.getFullYear();
 let dayName = dayNames[d.getDay()];
 let monthName = months[d.getMonth()];
 let fullDate = dayName + ", " + monthName + " " + d.getDate() + ", " + d.getFullYear() + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
 
 document.getElementById("currentDate").textContent = fullDate;
+document.querySelector("#currentYear").textContent = year;
 /**************************************** */
