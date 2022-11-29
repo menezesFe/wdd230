0b72df727b2d866c0f5b4dbe56387e0b184b3406
menezesFe <felipe.menezes6@outlook.com>
2022-11-29 17:52:12 -0500

    Fixed current date computation bug

--- chamber/js/date.js
+++ chamber/js/date.js
@@ -23,13 +23,17 @@ let months = [
 ];
 
 let d = new Date(document.lastModified);
-// let currentDay = new Date();
+let curDay = new Date();
 // console.log(currentDay);
 let year = d.getFullYear();
 let dayName = dayNames[d.getDay()];
 let monthName = months[d.getMonth()];
 let fullDate = d.getMonth() + "/" + d.getDate() + "/" + year + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-let spelledDate = dayName + ", " + d.getDate() + " " + monthName + " " + year;
+//current Dates
+let cDayName = dayNames[curDay.getDay()];
+let cMonthName = months[curDay.getMonth()];
+let cYear = curDay.getFullYear();
+let spelledDate = cDayName + ", " + curDay.getDate() + " " + cMonthName + " " + cYear;
 
 document.getElementById("currentDate").textContent = spelledDate;
 document.getElementById("modifiedDate").textContent = fullDate;
